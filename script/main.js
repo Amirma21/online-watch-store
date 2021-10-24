16386496eba17a9a9de27bd94a0ae5f4d8a3655c
amir maleki <amirmaleki21physics@gmail.com>
2021-10-24 22:27:53 +0330

    responsive navbar

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
 //selector
 const nav = document.querySelector("nav")
 const dynamicText = document.querySelector(".dynamic-text")
+const menuBtn = document.querySelector(".navbar-btn")
+const menu = document.querySelector(".responsive-menu")
+const closeMenuBtn = document.querySelector(".close-menu")
+const backdrop = document.querySelector(".backdrop")
 
 
 //navigation 
@@ -13,7 +17,7 @@ window.addEventListener("scroll", () => {
 
 // dynamic type in header
 
-const text = [" کیفیت ", "زیبایی ", "برندهای برتر"]
+const text = ["زیبایی", " برترین ها ", "تنوع ", "کیفیت"]
 
 let count = 0;  //nuber of array
 let index = 0;
@@ -26,11 +30,31 @@ const type = () => {
     letter = currentText.slice(0, index++)
     dynamicText.innerText = letter
     setTimeout(type, 400)
-    if(currentText === letter) {
-        count ++
+    if (currentText === letter) {
+        count++
         index = 0
     }
 }
 
 type()
+
+
+// responsive menu
+
+menuBtn.addEventListener("click",()=>{
+    menu.style.right = "0%"
+    backdrop.style.display = "block"
+
+})
+
+const closeMenu = () => { 
+    menu.style.right = "-100%"
+    backdrop.style.display = "none"
+
+}
+closeMenuBtn.addEventListener("click",closeMenu)
+backdrop.addEventListener("click",closeMenu)
+
+
+
 
